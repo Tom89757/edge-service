80a61a136bc2e88a8b88d867b10c0242ab41aaef
Tom89757 <2689980244@qq.com>
2023-09-04 21:34:19 +0800

    add login

---
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.polarbookshop.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author Tom89757
+ */
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(
+            ServerHttpSecurity http
+    ) {
+        return http
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated()) //所有请求都需要身份验证
+                .formLogin(Customizer.withDefaults()) //通过登录表单启用用户身份验证
+                .build();
+    }
+}
