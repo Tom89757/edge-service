62f26bae58b81b39f45967f59b2ab9ab41abd76b
Tom89757 <2689980244@qq.com>
2023-09-06 00:59:00 +0800

    add tests

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 /**
  * @author Tom89757
@@ -12,15 +22,52 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(
-            ServerHttpSecurity http
+            ServerHttpSecurity http,
+            ReactiveClientRegistrationRepository clientRegistrationRepository
     ) {
         return http
-                .authorizeExchange(exchange ->
-                        exchange.anyExchange().authenticated()) //所有请求都需要身份验证
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico")
+                        .permitAll()
+                        .pathMatchers(HttpMethod.GET, "/books/**")
+                        .permitAll()
+                        .anyExchange().authenticated()) //所有请求都需要身份验证
 //                .formLogin(Customizer.withDefaults()) //通过登录表单启用用户身份验证
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(
+                                new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+                        ))
                 .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(
+                        oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf.csrfTokenRepository(
+                        CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
                 .build();
+    }
+
+    private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
+            ReactiveClientRegistrationRepository clientRegistrationRepository
+    ) {
+        var oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(
+                        clientRegistrationRepository
+                );
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
+        return oidcLogoutSuccessHandler;
+    }
+
+    @Bean
+    WebFilter csrfWebFilter() {
+        return (exchange, chain) -> {
+            exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
+                Mono<CsrfToken> csrfToken =
+                        exchange.getAttribute(CsrfToken.class.getName());
+                return csrfToken != null ? csrfToken.then() : Mono.empty();
+            }));
+            return chain.filter(exchange);
+        };
     }
 }
